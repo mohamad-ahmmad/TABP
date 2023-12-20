549e4d84f4bf9fcfcce76923fc341578a91d4f8a
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-12-20 16:15:17 +0200

    Add Login query

---
+++ src/Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+
+namespace Application.Abstractions;
+
+public interface IJwtProvider<T> where T : BaseEntity
+{
+    string GenerateToken(T obj);
+}
+
---
+++ src/Application/Users/Queries/Login/LoginUserQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Queries.Login;
+
+public class LoginUserQueryValidator : AbstractValidator<LoginUserQuery>
+{
+    public LoginUserQueryValidator()
+    {
+        RuleFor(q => q.CredentialsDto.Username)
+            .NotEmpty();
+        RuleFor(q => q.CredentialsDto.Password)
+        .NotEmpty();
+    }
+}
+
