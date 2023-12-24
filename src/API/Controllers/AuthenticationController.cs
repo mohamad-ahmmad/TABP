023b18a2b961cb4bf334a659ee6bbaaa411508ce
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-12-24 12:06:26 +0200

    Add auth endpoint

---
+++ src/API/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Users.DTOs;
+using Application.Users.Queries.Login;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthenticationController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> AuthUserByCredentials(LoginCredentialsDto loginCredentialsDto, CancellationToken cancellationToken)
+        {
+            var loginQuery = new LoginUserQuery(loginCredentialsDto);
+            var result = await _mediator.Send(loginQuery, cancellationToken);
+            if (result.IsFailure)
+            {
+                return BadRequest(new
+                {
+                    result.Errors
+                });
+            }
+
+            return Ok(result.Response);
+        }
+    }
+}
