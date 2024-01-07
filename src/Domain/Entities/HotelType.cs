2cb6fad5c85a06390cff3a37ef727cf89469734a
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-01-07 17:35:18 +0200

    Add domain entites & migrations for hotel, hoteltype and owner

---
+++ src/Domain/Entities/HotelType.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class HotelType : BaseSoftDeletableAuditableEntity
+{
+    public string Type { get; set; } = string.Empty;
+    public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
+}
---
+++ src/Domain/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class Owner : BaseSoftDeletableEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
+}
+
