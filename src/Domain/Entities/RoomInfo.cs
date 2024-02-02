c4669d5f1689bc773f78acd00db7a6658b4bc1f4
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-02-02 18:03:19 +0200

    Delete PricePerDay field from room_infos

--- src/Domain/Entities/RoomInfo.cs
+++ src/Domain/Entities/RoomInfo.cs
@@ -5,7 +5,6 @@ public class RoomInfo : BaseSoftDeletableEntity
 {
     public int AdultsCapacity { get; set; }
     public int ChildrenCapacity { get; set; }
-    public int PricePerDay { get; set; } 
     public string Description { get; set; } = string.Empty;
     public Guid? RoomTypeId { get; set; }
     public RoomType? RoomType { get; set;} = null!;
