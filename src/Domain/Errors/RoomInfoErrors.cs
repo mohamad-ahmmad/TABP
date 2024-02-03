a81e4b6a69ddeaaeb50631ff9bf107be694e4d9c
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-02-03 14:42:35 +0200

    Add persistence logic related to RoomInfo Entity

---
+++ src/Domain/Errors/RoomInfoErrors.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Shared;
+
+namespace Domain.Errors;
+public static class RoomInfoErrors
+{
+    public readonly static Error NotFoundRoomInfo = new("Room Info", "Not found");
+}
+
---
+++ src/Infrastructure/Persistence/Repositories/RoomInfos/RoomInfosRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Domain.Errors;
+using Domain.Repositories;
+using Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Infrastructure.Persistence.Repositories.RoomInfos;
+public class RoomInfosRepository : IRoomInfosRepository
+{
+    private readonly TABPDbContext _dbContext;
+
+    public RoomInfosRepository(TABPDbContext dbContext)
+    {
+        _dbContext = dbContext;    
+    }
+
+    public async Task AddRoomInfoAsync(RoomInfo roomInfo, CancellationToken cancellationToken)
+    {
+        await _dbContext.AddAsync(roomInfo, cancellationToken);
+    }
+
+    public async Task<Result<object?>> DeleteRoomInfoByIdAsync(Guid roomInfoId, CancellationToken cancellationToken)
+    {
+        var roomInfo = await _dbContext.RoomInfos
+            .FirstOrDefaultAsync(ri => ri.Id == roomInfoId, cancellationToken);
+        if(roomInfo == null)
+        {
+            return Result<object?>.Failure(RoomInfoErrors.NotFoundRoomInfo, HttpStatusCode.NotFound);
+        }
+        roomInfo.IsDeleted = true;
+        return Result<object?>.Success(HttpStatusCode.NoContent);
+    }
+
+    public async Task<IEnumerable<RoomInfo>> GetAllRoomInfosAsync(Guid hotelId, CancellationToken cancellationToken)
+    {
+        var roomInfos = await _dbContext.RoomInfos
+            .Where(r => r.IsDeleted == false && r.HotelId == hotelId)
+            .ToListAsync(cancellationToken);
+
+        return roomInfos;
+    }
+}
+
