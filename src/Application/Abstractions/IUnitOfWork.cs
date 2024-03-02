05adc9c4a3269cefe4edc7c87be86f317f50dacb
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-03-02 20:43:05 +0200

    Add Transaction to UoW pattern

--- src/Application/Abstractions/IUnitOfWork.cs
+++ src/Application/Abstractions/IUnitOfWork.cs
@@ -1,7 +1,12 @@
-﻿namespace Application.Abstractions
+﻿using System.Data;
+
+namespace Application.Abstractions
 {
     public interface IUnitOfWork
     {
         Task<bool> CommitAsync(CancellationToken cancellationToken);
+        Task BeginTransactionAsync(IsolationLevel isolationLevel, CancellationToken cancellationToken);
+        Task CommitTransationAsync(CancellationToken cancellationToken);
+        Task RollBackTransactionAsync(CancellationToken cancellationToken);
     }
 }
--- src/Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
+++ src/Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Infrastructure.Persistence.UnitOfWork
 {
@@ -10,9 +12,35 @@ namespace Infrastructure.Persistence.UnitOfWork
         {
             _dbContext = dbContext;
         }
+
+        public async Task BeginTransactionAsync(IsolationLevel isolationLevel, CancellationToken cancellationToken)
+        {
+            await _dbContext.Database.BeginTransactionAsync(isolationLevel, cancellationToken);
+        }
+
         public async Task<bool> CommitAsync(CancellationToken cancellationToken)
         {
             return await _dbContext.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+        public async Task CommitTransationAsync(CancellationToken cancellationToken)
+        {
+            if(_dbContext.Database.CurrentTransaction == null)
+            {
+                return;
+            }
+
+            await _dbContext.Database.CurrentTransaction.CommitAsync(cancellationToken);
+        }
+
+        public async Task RollBackTransactionAsync(CancellationToken cancellationToken)
+        {
+            if(_dbContext.Database.CurrentTransaction == null)
+            {
+                return;
+            }
+
+            await _dbContext.Database.CurrentTransaction.RollbackAsync(cancellationToken);
         }
     }
 }
